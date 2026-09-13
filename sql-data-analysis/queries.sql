29d5cebb29bf932ea35c05ab42c30a21a6891654
Soheila Bozorgi <bozorgisoheila5@gmail.com>
2026-09-13 21:15:49 +0200

    Add all SQL practices into unified repository

---
+++ sql-data-analysis/queries.sql
@@ -0,0 +1,43 @@
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS orders;
+
+CREATE TABLE users (
+    user_id INT,
+    full_name VARCHAR(50)
+);
+
+CREATE TABLE orders (
+    order_id INT,
+    user_id INT,
+    amount INT,
+    order_date DATE
+);
+
+INSERT INTO users VALUES 
+(1, '  ali reza  '),
+(2, 'Sara M'),
+(3, 'mohammad');
+
+INSERT INTO orders VALUES 
+(101, 1, 600, '2026-08-01'),
+(102, 1, 200, '2026-08-10'),
+(103, 2, 800, '2026-08-02'),
+(104, 2, 1200, '2026-09-01'),
+(105, 3, 300, '2026-09-05');
+
+
+SELECT 
+    UPPER(TRIM(users.full_name)) AS USER_NAME,
+    SUM(orders.amount) AS  TOTAL_SPENT,
+    COUNT(CASE
+            WHEN orders.amount >= 500 THEN 1
+            ELSE NULL
+            END) as VIP_ORDERS_COUNT,
+    STRFTIME('%Y', orders.order_date) AS ORDER_YEAR,
+    DENSE_RANK() OVER(ORDER BY SUM(orders.amount) DESC) AS USER_RANK
+
+
+FROM users
+JOIN orders ON users.user_id = orders.user_id
+GROUP BY users.full_name, STRFTIME('%Y', orders.order_date);
+
